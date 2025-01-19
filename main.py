e4103b6f715a931d8880c92245fb70c32eaee6fd
ecmistry <49549389+ecmistry@users.noreply.github.com>
2025-01-19 07:54:30 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from commands.configure import set_url, set_token, show_config
 from commands.list_apis import list_all_apis
 from commands.create_api import create_api
 from commands.delete_api import delete_api
+from commands.update_api import update_api
 
 app = typer.Typer()
 
@@ -13,6 +14,7 @@ app.command(name="configure-show")(show_config)
 app.command(name="list-apis")(list_all_apis)
 app.command(name="create-api")(create_api)
 app.command(name="delete-api")(delete_api)
+app.command(name="update-api")(update_api)
 
 if __name__ == "__main__":
     app()
